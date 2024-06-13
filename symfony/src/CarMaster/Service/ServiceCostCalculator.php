ee38f51b03b830f101110c8740d3a04d44f16263
Mary <krylaangel@gmail.com>
2024-06-13 23:53:57 +0300

    solid3

--- symfony/src/CarMaster/Service/ServiceCostCalculator.php
+++ symfony/src/CarMaster/Service/ServiceCostCalculator.php
@@ -7,12 +7,6 @@ use App\CarMaster\Entity\ServiceOrder;
 
 class ServiceCostCalculator implements ServiceCostCalculatorInterface
 {
-    /*
-     * Створити клас-сервіс (приклад назви  - ServiceCostCalculator), який займається підрахунком вартості якогось,
-     * заздалегіть збереженого в БД, сервісного замовлення на авто . Клас має ралізувати метод по типу
-     * calculateTotalCost(): int і повертати ітогову ціну за всі роботи + запчастини (матеріали),
-     * використані в замолвені.
-     */
     public function serviceCostCalculator(ServiceOrder $serviceOrder): float
     {
         {
